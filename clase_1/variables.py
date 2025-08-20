a228b26627e3210b273b56585a842b6b68796810
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-08-19 21:07:48 -0300

    Add clase 1

--- clase_1/variables.py
+++ clase_1/variables.py
@@ -13,9 +13,11 @@ Reglas de las varibles:
 # snake case escribir_de_esta_manera
 # camel case escribirDeEstaManera
 # upper camel case EscribirDeEstaManera
+# Guia de estilos de google https://google.github.io/styleguide/pyguide.html
 
 
-_variable = 10
+_variable = 10 # Variable privada
+__variable = 10 # Variable MUY privada
 variable1 = 1
 
 edad = 33 # Tipo de dato numero (int) (integer) (entero)
@@ -51,3 +53,22 @@ Ejemplo:
 suma = 10 + 5
 print(suma)
 
+"""
+Operaciones aritmeticas basicas
+
+Suma            +
+Resta           -
+Multiplicacion  *
+Division        /
+
+"""
+
+suma = 1 + 1
+resta = 1 - 1
+multiplicar = 2 * 2
+dividir = 3 / 3
+
+print("Suma: ", suma)
+print("Resta: ", resta)
+print("Multiplicacion: ", multiplicar)
+print("Division: ", dividir)
