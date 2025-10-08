b8925d19039a8464587a3b9481e97d30770d9c36
Juan Ignacio Bonini <juanignaciobonini@hotmail.com>
2025-10-07 21:19:44 -0300

    Add examen explicación

--- simulacion_examen/enun.py
+++ simulacion_examen/enun.py
@@ -6,12 +6,15 @@ de cada componente (en mililitros). El objetivo del sistema es verificar si la m
 con las especificaciones de formulación, considerando un margen de tolerancia de ±5
 % para cada componente respecto de los valores teóricos.
 Se solicita desarrollar un programa en Python que:
+
 1. Solicite los volúmenes medidos de los tres componentes.
 2. Calcule los porcentajes reales a partir del total.
 3. Compare los resultados con los valores teóricos y determine si cada componente
 está dentro del rango permitido.
 4. Indique si el lote es APROBADO o RECHAZADO.
 5. Permite repetir el proceso para múltiples lotes mientras el usuario lo desee.
+
+
 El programa debe implementarse utilizando:
 ● Una función encargada de realizar el análisis y devolver el resultado.
 ● Un bucle while para la iteración.
--- simulacion_examen/examen.py
+++ simulacion_examen/examen.py
@@ -3,7 +3,7 @@
 # 300    1000 -> 30 %
 # 200    1000 -> 20 %
 
-def analizar_lote(volumenes):
+def analizar_lote(volumenes): # 1. Solicite los volúmenes medidos de los tres componentes.
     proporciones_teoricas = (50, 30, 20) # tupla
     tolerancia = 5
 
@@ -24,18 +24,23 @@ def analizar_lote(volumenes):
 
         # porcentaje_proporcion
         for proporcion in volumen:
-            porcentaje_proporcion = (proporcion / total) * 100
+            porcentaje_proporcion = (proporcion / total) * 100 # 2. Calcule los porcentajes reales a partir del total.
+            
+            # 3. Compare los resultados con los valores teóricos y determine si cada componente
+            # está dentro del rango permitido.
             esperado = proporciones_teoricas[cont]
             cont += 1
 
             minimo = esperado - tolerancia
             maximo = esperado + tolerancia
-            
             if porcentaje_proporcion >= minimo and porcentaje_proporcion <= maximo:
                 aprobado = True
             else:
                 aprobado = False
                 break     
+            # hasta acá
+        
+        # 4. Indique si el lote es APROBADO o RECHAZADO.
         if aprobado:
             dentro_rango += 1
         else:
@@ -43,5 +48,9 @@ def analizar_lote(volumenes):
     return fuera_rango == 0
 
 volumenes = [(500, 300, 200),(500, 300, 200), (500, 300, 200), (500, 300, 200)]
-print(analizar_lote(volumenes))
 
+# 5. Permite repetir el proceso para múltiples lotes mientras el usuario lo desee.
+print(analizar_lote(volumenes)) # 1. Solicite los volúmenes medidos de los tres componentes.
+
+volumenes = [(100,200,300), (100,200,300)]
+print(analizar_lote(volumenes)) # 1. Solicite los volúmenes medidos de los tres componentes.
